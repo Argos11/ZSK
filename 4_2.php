aa39b34810228371470fc5536a206ae4dad0841c
Ania_Modrzyńska <66321595+Argos11@users.noreply.github.com>
2021-09-28 09:35:11 +0200

    Add files via upload

---
+++ 4_2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Figury geometryczne</title>
+  </head>
+
+  <body>
+    <h1>Figury geometryczne</h1>
+    <form action="./scripts/4_2_scripts.php" method="post">
+      <input type="text" name="imie" placeholder="Podaj imię"><br>
+      <input type="radio" name="geometry" value = "kwadrat" >Kwadrat<br><br>
+      <input type="radio" name="geometry" value = "prostokat">Prostokąt<br><br>
+      <input type="submit" value="Zatwierdź figurę">
+    </form>
+
+  </body>
+</html>
