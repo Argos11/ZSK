a4d461caa7e99d242d74a3d0fe58f063c2ef3f1e
Ania_Modrzyńska <66321595+Argos11@users.noreply.github.com>
2021-10-19 09:36:42 +0200

    Add files via upload

---
+++ functions/function.php
@@ -0,0 +1,27 @@
+<?php
+  function show(){
+    echo "test";
+  }
+  function show1(){
+    return;
+  }
+
+  function showname($name){
+    return $name;
+  }
+
+  //zdefiniuj funkcje o nazwie stringvalidate, ktora wykona nastepujace dzialania
+//  1.usunie biale znaki
+//  2.zamieni pierwsza litere na duza
+//  3.od drugiej litery male
+//  4.drugi parametr bedzie okreslac dlugosc stringa
+
+ function stringvalidate($name, $long){
+   $name = trim($name);
+   $name = strtolower($name);
+   $name = ucfirst($name);
+   $name = substr($name, 0, $long);
+   //return substr(ucfirst(strtolower(trim($name))), 0, $long);
+   return $name;
+ }
+ ?>
